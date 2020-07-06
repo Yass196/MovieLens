7d21a2a8ac12291cd4c91742e3c0b6d179e37886
yassine <yassineyounsii@gmail.com>
2020-07-06 17:44:45 +0200

    adding ratings

--- src/main/java/Classification/MovieClassification.java
+++ src/main/java/Classification/MovieClassification.java
@@ -1,4 +1,19 @@
 package Classification;
 
 public class MovieClassification {
+
+/*
+    public static double similarity(int [] vector1, int [] vector2 ){
+        int somme = 0;
+        double cardV1 = 0.0;
+        double cardV2 = 0.0;
+        for(int i = 0; i<vector1.length; i++){
+            somme += vector1[i]*vector2[i];
+            cardV1 += Math.pow(vector1, 2)
+        }
+
+    }
+*/
+
+
 }
